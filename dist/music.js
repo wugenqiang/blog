c5280c4486cf83bf45c6b8a2371c083b14b515bc
wugenqiang <2422676183@qq.com>
2019-03-15 11:42:30 +0800

    Site updated: 2019-03-15 11:42:28

--- dist/music.js
+++ dist/music.js
@@ -7,7 +7,7 @@ const ap = new APlayer({
       name: '风筝误',
       artist: '刘珂矣',
       url: 'http://up.mcyt.net/?down/46644.mp3',
-      cover: 'http://oeff2vktt.bkt.clouddn.com/image/96.jpg',
+      cover: 'https://raw.githubusercontent.com/wugenqiang/myblog_album/master/photos/2019-03-14_13145201.jpg',
     },
     {
       name: '昨日青空',
@@ -26,6 +26,54 @@ const ap = new APlayer({
       artist: '刘增瞳',
       url: 'http://www.ytmp3.cn/down/49817.mp3',
       cover: 'http://img.ytmp3.cn/image/26.jpg',
+    },
+    {
+      name: '静悄悄',
+      artist: '大泫',
+      url: 'http://www.ytmp3.cn/down/55867.mp3',
+      cover: 'http://img.ytmp3.cn/image/28.jpg',
+    },
+    {
+      name: '越来越不懂',
+      artist: '蔡健雅',
+      url: 'http://www.ytmp3.cn/down/51502.mp3',
+      cover: 'http://img.ytmp3.cn/image/44.jpg',
+    },
+    {
+      name: '东西',
+      artist: '林俊呈',
+      url: 'http://www.ytmp3.cn/down/54720.mp3',
+      cover: 'https://raw.githubusercontent.com/wugenqiang/myblog_album/master/photos/2019-01-07_150.jpg',
+    },
+    {
+      name: '起风了',
+      artist: '蒙面哥',
+      url: 'http://www.ytmp3.cn/down/56965.mp3',
+      cover: 'http://img.ytmp3.cn/image/54.jpg',
+    },
+    {
+      name: '可能否',
+      artist: '洛晴',
+      url: 'http://www.ytmp3.cn/down/53934.mp3',
+      cover: 'http://img.ytmp3.cn/image/67.jpg',
+    },
+    {
+      name: '起风了',
+      artist: '大神慧',
+      url: 'http://www.ytmp3.cn/down/52305.mp3',
+      cover: 'http://img.ytmp3.cn/image/54.jpg',
+    },
+    {
+      name: '纸短情长',
+      artist: '烟把儿乐队',
+      url: 'http://www.ytmp3.cn/down/49110.mp3',
+      cover: 'https://raw.githubusercontent.com/wugenqiang/myblog_album/master/photos/2019-01-07_540.jpg',
+    },
+    {
+      name: '小半',
+      artist: '陈粒',
+      url: 'http://www.ytmp3.cn/down/52276.mp3',
+      cover: 'https://raw.githubusercontent.com/wugenqiang/myblog_album/master/photos/2019-02-18_1314520.jpg',
     }
   ]
 });
