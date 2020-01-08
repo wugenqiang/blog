96b3ddb31f1d49529829c2b5207198b507b64848
WuGenQiang <2422676183@qq.com>
2020-01-09 01:54:28 +0800

    Site updated: 2020-01-09 01:54:06

--- js/src/schemes/muse.js
+++ js/src/schemes/muse.js
@@ -1,3 +1,9 @@
-// build time:Wed Jan 08 2020 23:53:49 GMT+0800 (GMT+08:00)
-$(document).ready(function(){function t(){var t=$("#footer").attr("position")?$(".container").height()+$("#footer").outerHeight(true):$(".container").height();if(t<window.innerHeight)$("#footer").css({position:"fixed",bottom:0,left:0,right:0}).attr("position","fixed");else $("#footer").removeAttr("style position")}t();$(window).on("resize scroll",t)});
-//rebuild by neat 
+$(document).ready(function() {
+  function updateFooterPosition() {
+    var containerHeight = $('#footer').attr('position') ? $('.container').height() + $('#footer').outerHeight(true) : $('.container').height();
+    if (containerHeight < window.innerHeight) $('#footer').css({ 'position': 'fixed', 'bottom': 0, 'left': 0, 'right': 0 }).attr('position', 'fixed');
+    else $('#footer').removeAttr('style position');
+  }
+  updateFooterPosition();
+  $(window).on('resize scroll', updateFooterPosition);
+});
